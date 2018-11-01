b39fb3e6268c0a60b3bc2434cdd5b5ba4d9c4b2e
Daniel Lindegaard <Latupho@users.noreply.github.com>
2018-11-01 13:58:34 +0100

    Just because I'm lasy....
    Sometime it can be change to a function, so I just ask the consol, but not now... Was not allowed to get coffee...

---
+++ Can I get coffee to day.ps1
@@ -0,0 +1,6 @@
+﻿
+# Can I get a cub of coffie to day?
+
+[System.Array]$dataset = 'No','No','No','No','No','No','No','No','No','No','No','No','No','No','No','No','No','No','Yes','No','No','No','No','No','No','No','No','No'
+
+Get-Random -InputObject $dataset
